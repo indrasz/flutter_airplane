8982fcf8cc4564ab64f1447448cef9bfa19eaa99
wahyuindra <wahyuindra712@gmail.com>
2022-08-03 20:52:21 +0700

    add splash page

---
+++ lib/modules/splash/page/splash_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:airplane/utils/themes.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: EdgeInsets.only(bottom: 50),
+              child: SvgPicture.asset(
+                'assets/icon_logo.svg',
+              ),
+            ),
+            Text(
+              'AIRPLANE',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: medium,
+                letterSpacing: 10,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
