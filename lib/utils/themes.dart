355569efc934860ace22102371b9370cf549de1f
wahyuindra <wahyuindra712@gmail.com>
2022-08-03 20:32:37 +0700

    add font weight

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -35,4 +35,14 @@ TextStyle greyTextStyle = GoogleFonts.poppins(
   color: kGreyColor,
 );
 
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
+
+
+
 
